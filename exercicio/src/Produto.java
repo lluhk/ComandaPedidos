a7e37d68df9d9c2f28ae68c49feaf3d4553baded
Luan Carlos Muhlbeier <luan.muhlbeier@alunos.bpkedu.com.br>
2024-09-19 19:51:27 -0300

    att

---
+++ exercicio/src/Produto.java
@@ -0,0 +1,42 @@
+public class Produto {
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return
+                "nome='" + nome + '\'' +
+                ", preco=" + preco;
+    }
+}
